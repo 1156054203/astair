cd922882d1bf32a6640f62de95a08fc3994f9715
gvelikova <gergana_velikova@yahoo.com>
2019-01-25 14:45:28 +0000

    Renamed argument.

--- DNA_sequences_operations.py
+++ DNA_sequences_operations.py
@@ -1,11 +1,11 @@
-def complementary(self):
+def complementary(sequence):
     """Takes an input DNA string and gives its complementary."""
-    final_string = self.translate({ord("T"): "A", ord("A"): "T", ord("G"): "C", ord("C"): "G", ord("t"): "a", ord("a"): "t", ord("g"): "c", ord("c"): "g"})
+    final_string = sequence.translate({ord("T"): "A", ord("A"): "T", ord("G"): "C", ord("C"): "G", ord("t"): "a", ord("a"): "t", ord("g"): "c", ord("c"): "g"})
     return final_string
 
-def reverse_complementary(self):
+def reverse_complementary(sequence):
     """Takes an input DNA string and gives its reverse complementary."""
-    reverse_string = list(self)
+    reverse_string = list(sequence)
     reverse_string.reverse()
     final_string = "".join(reverse_string).translate({ord("T"): "A", ord("A"): "T", ord("G"): "C", ord("C"): "G", ord("t"): "a", ord("a"): "t", ord("g"): "c", ord("c"): "g"})
     return final_string
