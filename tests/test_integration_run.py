ba49c0f8622bf4a17f25e33a9d356eb17a106496
Benjamin Schuster-Boeckler <ben.sb@methodicate.com>
2019-03-11 14:09:38 +0000

    simplified and fixed run test

--- tests/test_integration_run.py
+++ tests/test_integration_run.py
@@ -17,9 +17,7 @@ class AsTaiRunTest(unittest.TestCase):
 
     def test_run(self):
         """"""
-        self.assertEqual(run.cli.help, 'asTair (tools for processing cytosine modification sequencing data)')
-        self.assertEqual(run.cli.epilog, '\n__________________________________About__________________________________\nasTair was written by Gergana V. Velikova and Benjamin Schuster-Boeckler.\nThis code is made available under the GNU General Public License, see \nLICENSE.txt for more details.\n\n                                                         Version: 3.0.0\n')
-        
+        self.assertEqual(run.cli.help, 'asTair (tools for processing cytosine modification sequencing data)')        
         
 if __name__ == '__main__':
     unittest.main()
