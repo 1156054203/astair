41859a39e5ff6a0d3618a2038fdba52d429ec856
gvelikova <gergana_velikova@yahoo.com>
2019-08-01 17:23:22 +0100

    Added more parameters to customise the aligner.

--- astair/aligner.py
+++ astair/aligner.py
@@ -46,13 +46,31 @@ from astair.simple_fasta_parser import fasta_splitting_by_sequence
 @click.option('end_clipping_penalty', '--end_clipping_penalty', '-L', default='5,5', type=str, required=False, help='The penalty for 5-prime- and 3-prime-end clipping, see BWA manual. (Default 5,5).')
 @click.option('unpaired_penalty', '--unpaired_penalty', '-U', default=17, type=int, required=False, help='The penalty for an unpaired read pair, see BWA manual. (Default 17).')
 @click.option('read_type', '--read_type', '-x', default='null', type=click.Choice(['null', 'pacbio', 'ont2d', 'intractg']), required=False, help='Changes multiple parameters unless overridden, see BWA manual. (Default null).')
+@click.option('smart_pairing', '--smart_pairing', '-smp', default=False, is_flag=True,  required=False, help='Ignores read2, see BWA manual.')
+@click.option('read_group', '--read_group', '-rg', default=None, type=str,  required=False, help='Adds the given read group line "@RG\\tID:ids\\tSM:name", see BWA manual. (Default None).')
+@click.option('header_string', '--header_string', '-hs', default=None, type=str,  required=False, help='Adds the given string to header if it starts with @, or to file if it does not, see BWA manual.')
+@click.option('include_alt', '--include_alt', '-al', default=False, is_flag=True,  required=False, help='Treats ALT contigs as part of the primary assembly, see BWA manual.')
+@click.option('split_alignment', '--split_alignment', '-spa', default=False, is_flag=True,  required=False, help='Takes the alignment with the smallest coordinate as primary in case of split alignment, see BWA manual.')
+@click.option('supplementary_mapq', '--supplementary_mapq', '-smq', default=False, is_flag=True,  required=False, help='Does not modify the MAPQ of supplementary alignments, see BWA manual.')
+@click.option('minimum_score', '--minimum_score', '-ms', default=30, type=int, required=False, help='Minimum score to ouput, see BWA manual. (Default 30).')
+@click.option('alternative_score', '--alternative_score', '-h', default='5,200', type=str, required=False, help='Defines the number of reads that will be tagged as XA if they have 80 percent of the maximum score, see BWA manual. (Default 5,200).')
+@click.option('all_alignments', '--all_alignments', '-aa', default=False, is_flag=True,  required=False, help='Outputs all SE or unpaired PE reads, see BWA manual.')    
+@click.option('fasta_comment', '--fasta_comment', '-fc', default=None,  required=False, help='Adds FASTA/FASTQ comment to output, see BWA manual.')   
+@click.option('fasta_header',  '--fasta_header', '-fh', default=None,  required=False, help='Outputs the reference header in the XR tag, see BWA manual.')   
+@click.option('clip_supplementary',  '--clip_supplementary', '-cs', default=False, is_flag=True,  required=False, help='Enables soft clipping on supplementary alignments, see BWA manual.')   
+@click.option('mark_splitted',  '--mark_splitted', '-ms', default=False, is_flag=True,  required=False, help='Labels short split reads as secondary, see BWA manual.')                                                               
+@click.option('reads_distribution', '--reads_distribution', '-rd', default=None, required=False, help='Specifies the mean, standard deviation (10 percent of the mean if absent), max (4 sigma from the mean if absent) and min of the insert size distribution for FR orientation, see BWA manual. Must be provided as FLOAT,FLOAT,INT,INT (Default reads distribution metrics are inferred from the data).')
+
+
+#smart_pairing, read_group, header_string, include_alt, split_alignment, supplementary_mapq, minimum_score, alternative_score, all_alignments, fasta_comment, fasta_header, clip_supplementary, mark_splitted, reads_distribution
+
 def align(fq1, fq2, reference, bwa_path, samtools_path, directory, method, output_format, minimum_mapping_quality, keep_unmapped, N_threads, minimum_seed_length, band_width, dropoff,
                  internal_seeds, reseeding_occurence, N_skip_seeds, drop_chains, discard_chains, N_mate_rescues, skip_mate_rescue, skip_pairing, match_score, mismatch_penalty,
-                 gap_open_penalty, gap_extension_penalty, end_clipping_penalty, unpaired_penalty, read_type, single_end):
+                 gap_open_penalty, gap_extension_penalty, end_clipping_penalty, unpaired_penalty, read_type, single_end, smart_pairing, read_group, header_string, include_alt, split_alignment, supplementary_mapq, minimum_score, alternative_score, all_alignments, fasta_comment, fasta_header, clip_supplementary, mark_splitted, reads_distribution):
     """Align raw reads in fastq format to a reference genome. bwa is required to align TAPS reads, and bwa-meth fif you plan to process BS-seq data."""
     run_alignment(fq1, fq2, reference, bwa_path, samtools_path, directory, method, output_format, minimum_mapping_quality, keep_unmapped, N_threads, minimum_seed_length, band_width, dropoff,
                  internal_seeds, reseeding_occurence, N_skip_seeds, drop_chains, discard_chains, N_mate_rescues, skip_mate_rescue, skip_pairing, match_score, mismatch_penalty,
-                 gap_open_penalty, gap_extension_penalty, end_clipping_penalty, unpaired_penalty, read_type, single_end)
+                 gap_open_penalty, gap_extension_penalty, end_clipping_penalty, unpaired_penalty, read_type, single_end, smart_pairing, read_group, header_string, include_alt, split_alignment, supplementary_mapq, minimum_score, alternative_score, all_alignments, fasta_comment, fasta_header, clip_supplementary, mark_splitted, reads_distribution)
 
 
 warnings.simplefilter(action='ignore', category=FutureWarning)
@@ -110,7 +128,7 @@ def check_index(use_bwa, reference, method, output_format):
 
 def run_alignment(fq1, fq2, reference, bwa_path, samtools_path, directory, method, output_format, minimum_mapping_quality, keep_unmapped, N_threads, minimum_seed_length, band_width, dropoff,
                  internal_seeds, reseeding_occurence, N_skip_seeds, drop_chains, discard_chains, N_mate_rescues, skip_mate_rescue, skip_pairing, match_score, mismatch_penalty,
-                 gap_open_penalty, gap_extension_penalty, end_clipping_penalty, unpaired_penalty, read_type, single_end):
+                 gap_open_penalty, gap_extension_penalty, end_clipping_penalty, unpaired_penalty, read_type, single_end, smart_pairing, read_group, header_string, include_alt, split_alignment, supplementary_mapq, minimum_score, alternative_score, all_alignments, fasta_comment, fasta_header, clip_supplementary, mark_splitted, reads_distribution):
     """Aligns the provided pair-end reads to the reference according to the method specified.
     Outputs a sorted and indexed file."""
     time_s = datetime.now()
@@ -150,17 +168,64 @@ def run_alignment(fq1, fq2, reference, bwa_path, samtools_path, directory, metho
         read_type = '-x intractg'
     if single_end == True or fq2 == None:
         fq2 = ''
+    if smart_pairing:
+        smart_pairing = ' -p '
+    else:
+        smart_pairing = ''
+    if read_group == None:
+        read_group = ''
+    else:
+        read_group = ' -R ' + read_group
+    if header_string == None:
+        header_string = ''
+    else:
+        header_string = ' -H ' + header_string
+    if include_alt:
+        include_alt = ' -j '
+    else:
+        include_alt = ''
+    if split_alignment:
+        split_alignment = ' -5 '
+    else:
+        split_alignment = ''
+    if supplementary_mapq:
+        supplementary_mapq = ' -q ' 
+    else:
+        supplementary_mapq = ''
+    if all_alignments:
+        all_alignments = ' -a '
+    else:
+        all_alignments = ''
+    if fasta_comment == None:
+        fasta_comment = ''
+    else:
+        fasta_comment = ' -C ' + fasta_comment
+    if fasta_header == None:
+        fasta_header = ''
+    else:
+        fasta_header = ' -V ' + fasta_header
+    if clip_supplementary:
+        clip_supplementary = ' -Y '
+    else:
+        clip_supplementary = ''
+    if mark_splitted:
+        mark_splitted = ' -M '
+    else:
+        mark_splitted = ''
+    if reads_distribution == None:
+        reads_distribution = ''
+    else:
+        reads_distribution = ' -I '+ reads_distribution
     if output_format == 'CRAM':
         output_f = '-hC'
     else:
         output_f = '-hb'
     if method == 'mCtoT':
         alignment_command = '{} {} -t {} -k {} -w {} -d {} -r {} -y {} -c {} -D {} -W {} -m {} {} ' \
-                            ' {} -A {} -B {} -O {} -E {} -L {} -U {} {} {} {} {} | {} view {} -T {} -q {} {} -O {} ' \
+                            ' {} -A {} -B {} -O {} -E {} -L {} -U {} {} {} {} {} {} {} {} -T {} -h {} {} {} {} {} {} {} {} {} {} | {} view {} -T {} -q {} {} -O {} ' \
                             '| {} sort -@ {} -O {} > {}'.format(use_bwa, 'mem', N_threads, minimum_seed_length, band_width, dropoff,
                  internal_seeds, reseeding_occurence, N_skip_seeds, drop_chains, discard_chains, N_mate_rescues, skip_mate_rescue, skip_pairing, match_score, mismatch_penalty,
-                 gap_open_penalty, gap_extension_penalty, end_clipping_penalty, unpaired_penalty, read_type,
-                   reference, fq1, fq2, use_samtools, output_f, reference, minimum_mapping_quality,
+                 gap_open_penalty, gap_extension_penalty, end_clipping_penalty, unpaired_penalty, read_type, smart_pairing, read_group, header_string, include_alt, split_alignment, supplementary_mapq, minimum_score, alternative_score, all_alignments, fasta_comment, fasta_header, clip_supplementary, mark_splitted, reads_distribution, reference, fq1, fq2, use_samtools, output_f, reference, minimum_mapping_quality,
                    aligned_string, output_format, use_samtools, N_threads, output_format, os.path.join(directory + name + '_' + method + "." + output_format.lower()))
     else:
         alignment_command = 'python {} -t {} --reference {} {} {} | {} view {} -T {} -q {} {} -O {} | {} sort -@ {} -O {} > {}'.\
